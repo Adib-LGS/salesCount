9076206474f09d08820db2957c7702596fea25bf
Adib-Legastelois <adib.legastelois@icloud.com>
2020-08-04 18:10:21 +0100

    Refacto + wire:poll.5s method to refresh components

--- app/Http/Livewire/SalesDashboard.php
+++ app/Http/Livewire/SalesDashboard.php
@@ -8,35 +8,12 @@ use App\Service\Stats;
 class SalesDashboard extends Component
 {
 
-    public $newOrders;
-
-    public $salesAmount;
-
-    public $satisfactions;
-
-    protected $listeners = ['fetchStats'];
-
-    public function mount()
-    {
-        return $this->assignStats();
-    }
-
-    public function fetchStats()
-    {
-        return $this->assignStats();
-    }
-
-
     public function render()
     {
-        return view('livewire.sales-dashboard');
-    }
-
-    private function assignStats()
-    {
-        $this->newOrders = Stats::newOrders();
-        $this->salesAmount = Stats::salesAmount();
-        $this->satisfactions = Stats::satisfactions();
-
+        return view('livewire.sales-dashboard', [
+            'newOrders' => Stats::newOrders(),
+            'salesAmount' => Stats::salesAmount(),
+            'satisfactions' => Stats::satisfactions()
+        ]);
     }
 }

982f52137fb296d3ea8509baeef80c7ee72163a5
Adib-Legastelois <adib.legastelois@icloud.com>
2020-08-04 17:54:16 +0100

    First commit

---
+++ app/Http/Livewire/SalesDashboard.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Service\Stats;
+
+class SalesDashboard extends Component
+{
+
+    public $newOrders;
+
+    public $salesAmount;
+
+    public $satisfactions;
+
+    protected $listeners = ['fetchStats' => 'refresh'];
+
+    public function mount()
+    {
+        return $this->assignStats();
+    }
+
+    public function refresh()
+    {
+        return $this->assignStats();
+    }
+
+
+    public function render()
+    {
+        return view('livewire.sales-dashboard');
+    }
+
+    private function assignStats()
+    {
+        $this->newOrders = Stats::newOrders();
+        $this->salesAmount = Stats::salesAmount();
+        $this->satisfactions = Stats::satisfactions();
+
+    }
+}
